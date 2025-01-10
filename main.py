ca0d01b5e84d17cfaa936a536753e0e21865971b
denizberkin-vit <deniz@vitamu.com>
2025-01-10 16:19:02 +0300

    basic samples

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import os
+
+# You can change the framework as you like
+# Only restriction is that you should be able to save, load and make predictions with it.
+import torch
+import torch.nn as nn
+
+
+# TO FILL
+def load_model(model_path: str):
+    """ Loads and returns model from given path """
+    return None
+
+
+# TO FILL
+def predict(sample) -> str:
+    """ 
+    Input: sample (str): single sample containing a movie review.
+    Returns: the sentiment type: ["positive", "negative"]
+    """
+    # load model, vectorizer
+    # preprocess sample
+    # predict
+    # return sentiment type: ["positive", "negative"]
+    return None
+
+
+# this is a pseudo test function to give an idea
+def test_model(test_sample: str, label: str) -> None:
+    result = predict(test_sample)
+    return result == label
+
